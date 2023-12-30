cc2f53c4adf926c5bccdd270690c0b44c80f8734
Ravsst2004 <ravindraviswanatha@gmail.com>
2023-12-30 19:55:13 +0800

    modify content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html, js}'],
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
   theme: {
     extend: {
       fontFamily: {
